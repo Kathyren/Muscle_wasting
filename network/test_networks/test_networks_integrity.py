714f06b5c0a83de6c918c372c4ad255c9bf26414
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2025-02-27 15:39:14 +0000

    Test  for the integrity of the nodes (relevant to add better the metadata)

--- network/test_networks/test_networks_integrity.py
+++ network/test_networks/test_networks_integrity.py
@@ -79,7 +79,7 @@ def test_nodes_integrity(monkeypatch):
     print(network)
     assert len(network.nodes()) == 17, f"The test network ALDOA should have 6 genes to start and 11 mirnas."
 
-def test_get_cytoscape_network(monkeypatch):
+def test_read_cytoscape_json(monkeypatch):
     """
     This test will cgheck that the nodes we start with are ok having the minimun properties.
     :param monkeypatch:
@@ -88,7 +88,7 @@ def test_get_cytoscape_network(monkeypatch):
     cytoscape_json = ct.read_cytoscape_json(cytoscape_file="network/test_networks/ALDOA_LDHA.cyjs")
     nodes, edges, relationships = ct.format_cytoscape_json(cytoscape_json=cytoscape_json)
 
-    
+    print(nodes)
     # check that nodes is len 6
     assert len(nodes) == 6, f"The test network ALDOA should have 6 nodes."
     # check that edges is len 0
@@ -105,3 +105,26 @@ def test_get_cytoscape_network(monkeypatch):
         assert 'type' in node, f"The node should have a type property"
         assert node['type'] in ['gene', 'mirna'], f"The node should have a type property with value gene or mirna"
 
+def test_get_cytoscape_network(monkeypatch):
+    nodes = [{'data': {'SUID': 2000278, 'id': '2000278',
+                       'id_original': '6071', 'name': 'NT5E',
+                       'selected': False, 'shared_name': 'NT5E'},
+              'position': {'x': 185.46056354998848, 'y': 296.23009851467975},
+              'selected': False, 'source': 'Cytoscape', 'type': 'gene'},
+             {'data': {'SUID': 2005417, 'id': '2005417',
+                       'id_original': '2645', 'name': 'ALDOA',
+                       'selected': False, 'shared_name': 'ALDOA'},
+              'position': {'x': 57.46056354998848, 'y': 124.04747060021452},
+              'selected': False, 'source': 'Cytoscape', 'type': 'gene'}]
+    edges = []
+    relationship= []
+
+    # monkeypatch to read_cytoscape_json to retun nodes, edges and relationship
+    monkeypatch.setattr("cytoscape.read_cytoscape_json", lambda *args, **kwargs: (nodes, edges, relationship))
+
+    network = network_processing.create_graph_from_dictionaries(nodes=nodes, edges=edges, relationship=relationship)
+    print (network)
+    assert len(network.nodes()) == 2, f"The network should have 2 nodes"
+    assert len(network.edges()) == 0, f"The network should have 0 edges"
+    assert network.nodes['NT5E']['type'] == 'gene', f"The node NT5E should have type gene"
+    assert network.nodes['ALDOA']['type'] == 'gene', f"The node ALDOA should have type gene"
